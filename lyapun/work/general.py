479d12a8b897ad9db9068de2174c65340d393fe9
Taras Lyapun <taraslyapun@gmail.com>
2012-12-08 16:07:40 +0200

    temp commit

--- lyapun/work/general.py
+++ lyapun/work/general.py
@@ -2,7 +2,14 @@
 
 
 def recieve_data_from_socket(connection):
-    data_length_bytes = connection.recv(2)
+    bytes = b''
+    while len(bytes) < 2:
+        bytes += connection.recv(1024)
+
+    data_length_bytes = bytes[:2]
     data_length = int.from_bytes(data_length_bytes, byteorder='big')
-    data = connection.recv(data_length)
-    return data
+    bytes = bytes[2:]
+
+    while len(bytes) < data_length:
+        bytes += connection.recv(1024)
+    return bytes
